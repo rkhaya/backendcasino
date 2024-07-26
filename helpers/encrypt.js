3b175dce1e45ca4a2bc13bc7b45d045219848345
rkhaya <61182873+rkhaya@users.noreply.github.com>
2024-07-25 22:56:48 -0500

    added limbo with provably fairness

--- helpers/encrypt.js
+++ helpers/encrypt.js
@@ -1,11 +1,19 @@
-const crypto = require('crypto');
+const crypto = require("crypto");
 // Example function to generate server seed (replace with your own method)
 function generateServerSeed() {
-    // Generate a random string as server seed (example)
-    return crypto.randomBytes(16).toString('hex');
-  }
+  // Generate a random string as server seed (example)
+  return crypto.randomBytes(16).toString("hex");
+}
 
-  module.exports = {
-    generateServerSeed
-  };
-  
+function getRandomMultiplier(clientSeed, serverSeed) {
+  const combinedSeed = serverSeed + "|" + clientSeed;
+  const hash = crypto.createHash("sha256").update(combinedSeed).digest("hex");
+  const rand = parseInt(hash.substring(0, 8), 16) / 0xffffffff;
+  const multiplier = Math.max(1, Math.exp(-Math.log(rand)));
+  return parseFloat(multiplier.toFixed(2));
+}
+
+module.exports = {
+  generateServerSeed,
+  getRandomMultiplier,
+};
